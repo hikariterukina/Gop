1dc06fd36c9df093a9a4fe2f90e127cb7fc61444
MYCOMPUTER\c17014 <c17014@std.it-college.ac.jp>
2018-06-23 16:28:09 +0900

    軽いプロトOK

--- Gop/Assets/terukinahikari/Scripts/Player.cs
+++ Gop/Assets/terukinahikari/Scripts/Player.cs
@@ -2,15 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    bool onu = true;
+    bool ond = true;
+    bool onr = true;
+    bool onl = true;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            onu = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            ond = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            onr = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            onl = false;
+        }
+        else
+        {
+            onu = true;
+            ond = true;
+            onr = true;
+            onl = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (onu == false && collision.gameObject.tag == "up")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (ond == false && collision.gameObject.tag == "down")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (onr == false && collision.gameObject.tag == "right")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (onl == false && collision.gameObject.tag == "left")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
--- Gop/Assets/terukinahikari/Scripts/Spawnpoint.cs
+++ Gop/Assets/terukinahikari/Scripts/Spawnpoint.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawnpoint : MonoBehaviour {
-    public GameObject CommandCube;
-    private int waitcount = 0;
+    public GameObject CommandCubeU;
+    public GameObject CommandCubeD;
+    public GameObject CommandCubeR;
+    public GameObject CommandCubeL;
+
+    private int waitcount = 0;                                                     
 
 
 	// Use this for initialization
@@ -15,9 +19,22 @@ public class Spawnpoint : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         waitcount += 1;
-        if (waitcount % 70  == 0)
+        int rcount = Random.Range(0, 4);
+        if (rcount == 0 && waitcount % 80  == 0)
         {
-            Instantiate(CommandCube, transform.position, transform.rotation);
+            Instantiate(CommandCubeU, transform.position, transform.rotation);
+        }
+        if (rcount == 1 && waitcount % 80 == 0)
+        {
+            Instantiate(CommandCubeD, transform.position, transform.rotation);
+        }
+        if (rcount == 2 && waitcount % 80 == 0)
+        {
+            Instantiate(CommandCubeR, transform.position, transform.rotation);
+        }
+        if (rcount == 3 && waitcount % 80 == 0)
+        {
+            Instantiate(CommandCubeL, transform.position, transform.rotation);
         }
     }
 }
