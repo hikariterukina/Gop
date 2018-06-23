dd311d3d0f3814624c247b535131a1d0876549b6
MYCOMPUTER\c17014 <c17014@std.it-college.ac.jp>
2018-06-23 13:13:44 +0900

    開発開始

---
+++ Gop/Assets/terukinahikari/Scripts/Spawnpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour {
+    public GameObject CommandCube;
+    private int waitcount = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        waitcount += 1;
+        if (waitcount % 12 == 0)
+        {
+            Instantiate(CommandCube, transform.position, transform.rotation);
+        }
+    }
+}
