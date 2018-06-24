292580b1c1a048a2d1a071a134ae6ec2a041030c
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-06-24 10:18:19 +0900

    フォント、タイトルアニメーションの追加

---
+++ Gop/Assets/katsuki/cokkie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cokkie : MonoBehaviour {
+    public bool rev;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!rev)
+        {
+            transform.Translate(-2, 0, 0);
+            if (transform.position.x <= -2075 * 2)
+            {
+                Vector2 v = transform.position;
+                v.x = 2075;
+                transform.position = v;
+            }
+        }
+        else
+        {
+            transform.Translate(2, 0, 0);
+            if(transform.position.x >= 2075)
+            {
+                Vector2 v = transform.position;
+                v.x = -2075;
+                transform.position = v;
+            }
+        }
+	}
+}
