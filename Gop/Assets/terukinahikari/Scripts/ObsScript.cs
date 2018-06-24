26a228e28c02d939546fd201dfd6785b9e4e4190
MYCOMPUTER\c17014 <c17014@std.it-college.ac.jp>
2018-06-24 15:11:55 +0900

    レベルをうまく調整

--- Gop/Assets/terukinahikari/Scripts/ObsScript.cs
+++ Gop/Assets/terukinahikari/Scripts/ObsScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class ObsScript : MonoBehaviour {
 
-    private float ObsSpeed = 100;
+    private float ObsSpeed = 50;
+    private int speedcount;
 	// Use this for initialization
 	void Start () {
+        Destroy(gameObject, 8.0f);
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.position += transform.forward * ObsSpeed * Time.deltaTime;
+	void Update ()
+    {
+        speedcount = Random.Range(-4, 4);
+        Vector2 pos = transform.position;
+        pos.x -= speedcount *7* ObsSpeed * Time.deltaTime;
+        pos.y += ObsSpeed * Time.deltaTime;
+        transform.position = pos;
 		
 	}
 }
